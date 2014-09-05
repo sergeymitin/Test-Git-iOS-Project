63f83365514a5bc7890b6a20544b691dfd5c8aae
Sergey Mitin <SMitin@iMac-Sergey.local>
2014-09-05 15:29:08 +0400

    second commit

---
+++ Git Test/SMNewFile.h
@@ -0,0 +1,13 @@
+//
+//  SMNewFile.h
+//  Git Test
+//
+//  Created by Sergey Mitin on 05.09.14.
+//  Copyright (c) 2014 Sergey Mitin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMNewFile : NSObject
+
+@end
---
+++ Git Test/SMNewFile.m
@@ -0,0 +1,13 @@
+//
+//  SMNewFile.m
+//  Git Test
+//
+//  Created by Sergey Mitin on 05.09.14.
+//  Copyright (c) 2014 Sergey Mitin. All rights reserved.
+//
+
+#import "SMNewFile.h"
+
+@implementation SMNewFile
+
+@end
